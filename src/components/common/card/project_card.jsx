278b1dcf0ba43839d090fa3a30c8b3eedcc677ea
sebastiap <sebastiap@gmail.com>
2022-04-19 22:36:29 -0300

    project Cards

--- src/components/common/card/project_card.jsx
+++ src/components/common/card/project_card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Avatar from "./avatar";
+import classes from './card.module.css';
 
 //incorporo esto para renderizar de forma segura
 // dangerouslySetInnerHTML no es muy seguro como su nombre lo indica
@@ -8,16 +9,18 @@ import DOMPurify from 'dompurify'
 function Card(props) {
     var tel = props.tel;
   return (
-    <div className="card">
-      <div className="top">
+    <div className={classes.proyect}>
+    <div className={classes.whitecard}>
+      <div className={classes.top}>
         <h2 className="name">{props.name}</h2>
-        <Avatar pic={props.pic} />
+        {/* <Avatar pic={props.pic} /> */}
       </div>
-      <div className="bottom">
+      <div className={classes.bottom}>
         {/* <Detail info={props.tel} /> */}
         {/* <Detail info={props.mail} /> */}
-        <div className="content" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(tel)}}></div>
+        <div className={classes.content} dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(tel)}}></div>
       </div>
+    </div>
     </div>
   );
 }
