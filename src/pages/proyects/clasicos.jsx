a6245c636fa54bd7239d6912079b3cc6d869c7d0
sebastiap <sebastiap@gmail.com>
2022-04-25 22:52:29 -0300

    page placeholder

--- src/pages/proyects/clasicos.jsx
+++ src/pages/proyects/clasicos.jsx
@@ -1,16 +1,16 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-const Clasicos = () =>
+const NotFound = () =>
 {
     return(
         <div className="notfound">
-            <h2>Pagina no encontrada</h2>
+            <h1>Pagina no encontrada</h1>
             <p> Esa pagina no puede encontrarse</p>
             <img src="https://http.cat/404" alt="Hidden Cat"/>
-            <p> <Link to="/">Volver al home </Link></p>
+            <p> <Link to="/spika-games/">Volver al home </Link></p>
         </div>
     );
 };
 
-export default Clasicos;
+export default NotFound;
